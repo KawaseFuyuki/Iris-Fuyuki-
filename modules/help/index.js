4cb82a4213ba035cd85af2c861f2b5cc76b1b51d
KawaseFuyuki <kawasefuyuki@gmail.com>
2026-05-14 00:16:36 +0530

    Update embed image URL in help module

--- modules/help/index.js
+++ modules/help/index.js
@@ -31,7 +31,7 @@ export async function handleHelp(message, args, config, client) {
         value: '`ping` `snipe` `stoptimer <id>` `antilink` `antinuke`',
       },
     )
-    .setImage('https://media1.tenor.com/m/0O7kPpdBghEAAAAC/%E0%B8%9D%E0%B8%99.gif')
+    .setImage('https://cdn.discordapp.com/attachments/1281623049757786163/1504192891545977044/IMG_20260429_115437_694.jpg?ex=6a06184b&is=6a04c6cb&hm=c5399c3461784f55f2b73753523e7c346cc02f399879de376a14fe68b6c312c4&')
     .setFooter({ text: `Iris Fuyuki 24/7 • Prefix: ${config.prefix || '&'}` });
 
   return message.reply({ embeds: [embed] });
